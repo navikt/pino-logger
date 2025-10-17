85a8ff597344f6695b8c10451f2cba6b05cf2c06
Karl J. Overå <k@rl.run>
2025-10-17 15:53:40 +0200

    feat: remove 'unused' grafana hacks

--- libs/pino-logger/src/logger.ts
+++ libs/pino-logger/src/logger.ts
@@ -1,28 +1,15 @@
 import pino, { DestinationStream, LoggerOptions } from 'pino'
 
-export const createLogger = (defaultConfig: LoggerOptions = {}, destination?: DestinationStream): pino.Logger =>
-    pino(
+export const createLogger = (defaultConfig: LoggerOptions = {}, destination?: DestinationStream): pino.Logger => {
+    return pino(
         {
             ...defaultConfig,
             timestamp: false,
             messageKey: 'message',
             formatters: {
-                level: (label) => {
-                    return { level: label }
-                },
-                log: (object: any) => {
-                    if (object.err) {
-                        // backendlogger has an Error-instance, frontendlogger has already serialized it
-                        const err = object.err instanceof Error ? pino.stdSerializers.err(object.err) : object.err
-                        object.stack_trace = err.stack
-                        object.type = err.type
-                        object.message = err.message
-                        delete object.err
-                    }
-
-                    return object
-                },
+                level: (label) => ({ level: label }),
             },
         },
         destination,
     )
+}
