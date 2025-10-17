79d71fa1d6226d24fffa93983e3ae4dac59b0ee6
Karl J. Overå <k@rl.run>
2025-10-17 15:53:40 +0200

    feat: remove 'unused' grafana hacks

--- libs/pino-logger/src/logger.ts
+++ libs/pino-logger/src/logger.ts
@@ -4,24 +4,10 @@ export const createLogger = (defaultConfig: LoggerOptions = {}, destination?: De
     pino(
         {
             ...defaultConfig,
-            timestamp: false,
+            timestamp: pino.stdTimeFunctions.isoTime,
             messageKey: 'message',
             formatters: {
-                level: (label) => {
-                    return { level: label }
-                },
-                log: (object: any) => {
-                    if (object.err) {
-                        // backendlogger has an Error-instance, frontendlogger has already serialized it
-                        const err = object.err instanceof Error ? pino.stdSerializers.err(object.err) : object.err
-                        object.stack_trace = err.stack
-                        object.type = err.type
-                        object.message = err.message
-                        delete object.err
-                    }
-
-                    return object
-                },
+                level: (label) => ({ level: label }),
             },
         },
         destination,
