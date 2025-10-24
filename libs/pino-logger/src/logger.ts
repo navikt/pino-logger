bcb17ed40984b25d24584ab66188b8586c305cd0
Karl J. Overå <k@rl.run>
2025-10-24 10:41:43 +0200

    feat: use pino mixins to get span_id and trace_id

--- libs/pino-logger/src/logger.ts
+++ libs/pino-logger/src/logger.ts
@@ -1,4 +1,5 @@
 import pino, { DestinationStream, LoggerOptions } from 'pino'
+import { context, isSpanContextValid, trace } from '@opentelemetry/api'
 
 export const createLogger = (defaultConfig: LoggerOptions = {}, destination?: DestinationStream): pino.Logger =>
     pino(
@@ -8,6 +9,19 @@ export const createLogger = (defaultConfig: LoggerOptions = {}, destination?: De
             messageKey: 'message',
             formatters: {
                 level: (label) => ({ level: label }),
+            },
+            mixin: () => {
+                const span = trace.getSpan(context.active())
+                if (!span) return {}
+
+                const spanContext = span.spanContext()
+                if (!isSpanContextValid(spanContext)) return {}
+
+                return {
+                    trace_id: spanContext.traceId,
+                    span_id: spanContext.spanId,
+                    trace_flags: `0${spanContext.traceFlags.toString(16)}`,
+                }
             },
         },
         destination,
