45856f1cdcc0b13c5f42f674f4513db29838d9a3
Karl J. Overå <k@rl.run>
2025-10-24 11:32:52 +0200

    chore: revert object mapping changes

--- libs/pino-logger/src/logger.ts
+++ libs/pino-logger/src/logger.ts
@@ -9,6 +9,18 @@ export const createLogger = (defaultConfig: LoggerOptions = {}, destination?: De
             messageKey: 'message',
             formatters: {
                 level: (label) => ({ level: label }),
+                log: (object: any) => {
+                    if (object.err) {
+                        // backendlogger has an Error-instance, frontendlogger has already serialized it
+                        const err = object.err instanceof Error ? pino.stdSerializers.err(object.err) : object.err
+                        object.stack_trace = err.stack
+                        object.type = err.type
+                        object.message = err.message
+                        delete object.err
+                    }
+
+                    return object
+                },
             },
             mixin: () => {
                 const span = trace.getSpan(context.active())
